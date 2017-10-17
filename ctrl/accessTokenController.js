53a73fd0e9d2c315a348c76e69305a3e27f5455c
麦砚 <maiyan@ehousechina.com>
2017-10-17 21:05:41 +0800

    accessToken

--- ctrl/accessTokenController.js
+++ ctrl/accessTokenController.js
@@ -1,12 +1,34 @@
 const { wechatService } = require('../service')
 const CONFIG = require('../config')
+const cache = require('memory-cache');
+
+// const CACHE_TIME = (60 + 50) * 1000
+const CACHE_TIME =  ( 50 + 60 ) * 60 * 1000 //微信 access_token的有效期目前为2个小时
 
 module.exports = {
-    async getAccessToken(req, res, next){
+    async getAccessToken(req, res, next) {
         const grant_type = CONFIG.WECHAT.GRANT_TYPE
         const appid = CONFIG.WECHAT.APPID
         const secret = CONFIG.WECHAT.SECRET
+        const cacheData = cache.get('accessToken')
+
+        if (cacheData) {
+            return res.json({
+                code: 1,
+                data: cacheData
+            })
+        }
+
         const data = await wechatService.getAccessToken(grant_type, appid, secret)
-        console.log(data);
+
+        cache.put('accessToken', data, CACHE_TIME, function (key, value) {
+            console.log(key, value);
+        });
+
+        return res.json({
+            code: 1,
+            data,
+        })
+
     }
 }
