8ff27f13003a9d4d929930a2be2885f1f1dc5587
麦砚 <maiyan@ehousechina.com>
2017-10-19 14:03:04 +0800

    调整错误处理

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const CONFIG = require('./config');
 
-const path = require( 'path' );
-const express = require( 'express' );
+const path = require('path');
+const express = require('express');
 
-const mongoose = require( 'mongoose' );
-const bodyParser = require( 'body-parser' );
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 // const logger = require( 'morgan' )
 // const auth = require('./app/midware/auth');
 
@@ -13,13 +13,13 @@ const router = require('./routes')
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect( CONFIG.MONGO_DB )
+mongoose.connect(CONFIG.MONGO_DB)
 
-  
-app.locals.moment = require( 'moment' );
-    
-app.use( bodyParser.urlencoded( {extended: false} ) )
-app.use( bodyParser.json() )
+
+app.locals.moment = require('moment');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 // 检验ts和token
@@ -28,18 +28,19 @@ app.use( bodyParser.json() )
 // 路由
 app.use(router) // router
 
+// 404
+app.use(function (req, res, next) {
+    next(new Error('Not Found'));
+});
+
 // 统一错误处理
-
-app.use((err, req, res, next) => { // eslint-disable-line
-
-    res.json({ code: -1, msg: err.message })
-
+app.use(({ code = -1, message, stack }, req, res, next) => { // eslint-disable-line
+    res.json({ code, msg: message })
+    if (req.method === 'OPTIONS') return
     console.log(err);
-    // if (code > 10005 || req.method === 'OPTIONS') return
-
 })
 
-app.listen( CONFIG.PORT );
+app.listen(CONFIG.PORT);
 
-console.log( 'wechatpublicplatform started on PORT ' + CONFIG.PORT );
+console.log('wechatpublicplatform started on PORT ' + CONFIG.PORT);
 
